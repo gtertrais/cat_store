92a19ad3ef6e60ba7ed47212e34bec75dd5c5f6c
BEECECE <be.charrier@yahoo.com>
2019-09-02 16:03:35 +0200

    after merging. avec seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'home/index'
-=======
   devise_for :users
   resources :items
   root 'items#index'
->>>>>>> development
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 2019_09_02_125313) do
->>>>>>> development
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-
-
-
-  
-=======
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,5 +41,4 @@ ActiveRecord::Schema.define(version: 2019_09_02_125313) do
     t.datetime "updated_at", null: false
   end
 
->>>>>>> development
 end
