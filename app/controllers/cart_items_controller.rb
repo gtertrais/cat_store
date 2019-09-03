d00ac27461d1927200491a4f75f18dda55892c00
adrien66 <adrien66.pub@gmail.com>
2019-09-03 18:50:13 +0200

    maj

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,53 +1,15 @@
 class CartItemsController < ApplicationController
-  before_action :set_cart_item, only: [:show, :edit, :update, :destroy]
-
-  # GET /cart_items
-  # GET /cart_items.json
-  def index
-    @cart_items = CartItem.all
-  end
-
-  # GET /cart_items/1
-  # GET /cart_items/1.json
-  def show
-  end
-
-  # GET /cart_items/new
-  def new
-    @cart_item = CartItem.new
-  end
-
-  # GET /cart_items/1/edit
-  def edit
-  end
-
-  # POST /cart_items
-  # POST /cart_items.json
+  before_action :set_cart, only: [:create, :destroy], :if => :signed_in?
+  
+  
   def create
-    @cart_item = CartItem.new(cart_item_params)
-
-    respond_to do |format|
-      if @cart_item.save
-        format.html { redirect_to @cart_item, notice: 'Cart item was successfully created.' }
-        format.json { render :show, status: :created, location: @cart_item }
-      else
-        format.html { render :new }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /cart_items/1
-  # PATCH/PUT /cart_items/1.json
-  def update
-    respond_to do |format|
-      if @cart_item.update(cart_item_params)
-        format.html { redirect_to @cart_item, notice: 'Cart item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
-      end
+    @cart.add_item(params)
+     
+    if @cart.save
+      redirect_to cart_path
+    else
+      flash[:error] = 'There was a problem adding this item to your cart.'
+      redirect_to root_path
     end
   end
 
@@ -55,20 +17,7 @@ class CartItemsController < ApplicationController
   # DELETE /cart_items/1.json
   def destroy
     @cart_item.destroy
-    respond_to do |format|
-      format.html { redirect_to cart_items_url, notice: 'Cart item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to cart_path
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart_item
-      @cart_item = CartItem.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_item_params
-      params.fetch(:cart_item, {})
-    end
 end
