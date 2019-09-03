eaad0bd3a97a118f00172678d0e9c8b8f450fe54
Gaspard49 <gaspard.tertrais@gmail.com>
2019-09-03 20:22:19 +0200

    merge

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,23 +1,22 @@
 class CartItemsController < ApplicationController
-  before_action :set_cart, only: [:create, :destroy], :if => :signed_in?
-  
-  
+  before_action :set_cart, only: [:create, :destroy]
+
+  # GET /cart_items
+  # GET /cart_items.json
+  def index
+    @cart_items = CartItem.all
+  end
+
+
+   
   def create
     @cart.add_item(params)
      
     if @cart.save
-      redirect_to cart_path
+      redirect_to carts_path
     else
       flash[:error] = 'There was a problem adding this item to your cart.'
-      redirect_to root_path
+      redirect_to @card
     end
   end
-
-  # DELETE /cart_items/1
-  # DELETE /cart_items/1.json
-  def destroy
-    @cart_item.destroy
-    redirect_to cart_path
-  end
-
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,14 +3,12 @@ class Cart < ApplicationRecord
     has_many :cart_items
     has_many :items, through: :cart_items
 
-
-    
     def add_item(item_params)
-        current_item = cart.item.find_by(item_id: item_params[:item][:item_id])
+        current_item = CartItem.find_by(item_id: items_params[:item][:item_id])
         if current_item
             flash[:alert] = "Item already in your basket"
         else
-            new_item = cart_item.create(item_id: item_params[:item][:item_id], cart_id: self.id)
+            new_item = CartItem.create(item_id: items_params[:item][:item_id], cart_id: self.id)
         end
     end
 end
